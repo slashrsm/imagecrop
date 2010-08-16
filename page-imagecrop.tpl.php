41b903b907f82c5c22af69f0daf886b255fb07ad
Nils destoop <zuuperman@361625.no-reply.drupal.org>
2010-08-16 08:35:17 +0000

    Make page tpl xhtml strict

--- page-imagecrop.tpl.php
+++ page-imagecrop.tpl.php
@@ -7,7 +7,8 @@
 
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 ?>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language ?>" xml:lang="<?php print $language->language ?>">
 <?php
   print $head;
   print $scripts;
