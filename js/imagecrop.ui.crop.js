e40112f5e4c7279ed0355bdfb62224df16d83396
Nils destoop <zuuperman@361625.no-reply.drupal.org>
2011-01-11 22:09:40 +0000

    Start from imagecrop ui and saving from it

--- js/imagecrop.ui.crop.js
+++ js/imagecrop.ui.crop.js
@@ -8,30 +8,39 @@ $(function () {
   Drupal.Imagecrop.cropUi.initControls();
 });  
 
+Drupal.Imagecrop.imageCropWidthField = null;
+Drupal.Imagecrop.imageCropHeightField = null;
+Drupal.Imagecrop.imageCropXField = null;
+Drupal.Imagecrop.imageCropYField = null;
+
 /**
  * Init the controls.
  */
 Drupal.Imagecrop.cropUi.initControls = function() {
   
+  Drupal.Imagecrop.imageCropWidthField = $('input[name="image-crop-width"]');
+  Drupal.Imagecrop.imageCropHeightField = $('input[name="image-crop-height"]');
+  Drupal.Imagecrop.imageCropXField = $('input[name="image-crop-x"]');
+  Drupal.Imagecrop.imageCropYField = $('input[name="image-crop-y"]');
+  
   if ($('#resizeMe').resizable) { 
-    
-    $('#resizeMe').resizable({
-    containment: $('#image-crop-container'),
-    //transparent: true,
-    aspectRatio: Drupal.settings.imagecrop.aspectRatio,
-    autohide: true,
-    handles: 'n, e, s, w, ne, se, sw, nw',
 
-    resize: function(e, ui) {
+    $('#resizeMe').resizable({
+      containment: $('#image-crop-container'),
+      aspectRatio: Drupal.settings.imagecrop.aspectRatio,
+      autohide: true,
+      handles: 'n, e, s, w, ne, se, sw, nw',
+
+      resize: function(e, ui) {
         this.style.backgroundPosition = '-' + (ui.position.left) + 'px -' + (ui.position.top) + 'px';
-      $("#edit-image-crop-width").val($('#resizeMe').width());
-      $("#edit-image-crop-height").val($('#resizeMe').height());
-      $("#edit-image-crop-x").val(ui.position.left);
-      $("#edit-image-crop-y").val(ui.position.top);
-        },
-    stop: function(e, ui) {
+        Drupal.Imagecrop.imageCropWidthField.val($('#resizeMe').width());
+        Drupal.Imagecrop.imageCropHeightField.val($('#resizeMe').height());
+        Drupal.Imagecrop.imageCropXField.val(ui.position.left);
+        Drupal.Imagecrop.imageCropYField.val(ui.position.top);
+      },
+      stop: function(e, ui) {
         this.style.backgroundPosition = '-' + (ui.position.left) + 'px -' + (ui.position.top) + 'px';
-        }
+      }
     });
     
   }
