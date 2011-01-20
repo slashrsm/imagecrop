9f065141e905278369d21c5599a43235e9882e71
Nils destoop <zuuperman@361625.no-reply.drupal.org>
2011-01-20 09:16:43 +0000

    Write generated image for cropping ui

--- imagecrop-crop-ui.tpl.php
+++ imagecrop-crop-ui.tpl.php
@@ -12,7 +12,7 @@ print drupal_render($style_selection);
 print drupal_render($settings_form);
 ?>
 <div id="imagecrop-crop-wrapper" style="width: <?php print $imagecrop->getImageWidth() ?>px; height: <?php print $imagecrop->getImageHeight() ?>px;">
-  <div id="image-crop-container" style="background-image: url('<?php print $imagecrop->getFile()->filepath ?>'); width:<?php print $imagecrop->getImageWidth() ?>px; height:<?php print $imagecrop->getImageHeight() ?>px;"></div>
-  <div id="resizeMe" style="background-image: url('<?php print $imagecrop->getFile()->filepath ?>'); width:<?php print $imagecrop->getWidth() ?>px; height:<?php print $imagecrop->getHeight() ?>px; top: 20px;"></div>
+  <div id="image-crop-container" style="background-image: url('<?php print $imagecrop->getCropDestination(TRUE); ?>'); width:<?php print $imagecrop->getImageWidth() ?>px; height:<?php print $imagecrop->getImageHeight() ?>px;"></div>
+  <div id="resizeMe" style="background-image: url('<?php print $imagecrop->getCropDestination(TRUE); ?>'); width:<?php print $imagecrop->getWidth() ?>px; height:<?php print $imagecrop->getHeight() ?>px; top: 20px;"></div>
 </div>
 
