320dfd1313d9b97025bfaef7e4323b0648ec07bc
zuuperman <nils.destoop@gmail.com>
2012-03-21 21:45:22 +0100

    Fixed media support: media 1.x and media 2.x now both work

--- js/imagecrop_field.js
+++ js/imagecrop_field.js
@@ -6,106 +6,43 @@
 
 (function ($) {
 
+	Drupal.Imagecrop = {}
+	
 /**
  * Loads media browsers and callbacks, specifically for media as a field.
  * This is overridden, because there is no js event available to hook in.
  */
-Drupal.behaviors.mediaElement = {
+Drupal.behaviors.imagecropMediaElement = {
     
   attach: function (context, settings) {  
-    
-    $('.media-widget', context).once('mediaBrowserLaunch', function () {
-      
-      var options = settings.media.elements[this.id];
-      globalOptions = {};
-      if (options.global != undefined) {
-        var globalOptions = options.global;
-      }
-      //options = Drupal.settings.media.fields[this.id];
-      var fidField = $('.fid', this);
-      var previewField = $('.preview', this);
-      var removeButton = $('.remove', this); // Actually a link, but looks like a button.
-      var imagecropDiv = $('.imagecrop', this);
-      var imagecropLink = imagecropDiv.find('a');
-      
-      // Show the Remove button if there's an already selected media.
-      if (fidField.val() != 0) {
-        removeButton.css('display', 'inline-block');
-      }
-  
-      // When someone clicks the link to pick media (or clicks on an existing thumbnail)
-      $('.launcher', this).bind('click', function () {
-        // Launch the browser, providing the following callback function
-        // @TODO: This should not be an anomyous function.
-        Drupal.media.popups.mediaBrowser(function (mediaFiles) {
-        	
-          if (mediaFiles.length < 0) {
-            return;
-          }
-          var mediaFile = mediaFiles[0];
-          
-          // Set the value of the filefield fid (hidden).
-          fidField.val(mediaFile.fid);
-          
-          // Set the preview field HTML.
-          previewField.html(mediaFile.preview);
-          // Show the Remove button.
-          removeButton.show();
-          
-          // Show the imagecrop link
-          imagecropDiv.css({display : 'inline-block'});
-
-          oldFid = 0;
-          if (mediaFile.type != 'image') {
-            imagecropLink.hide();
-          }
-          else {
-            
-            var oldHref = imagecropLink.attr('href');
-            var hrefParts = oldHref.split('/');
-          	for (var i in hrefParts) {
-          		if (hrefParts[i] == 'crop') {
-          			oldFid = hrefParts[parseInt(i) + 1];
-          			break;
-          		}
-          	}
-
-            // Set correct file
-            imagecropLink.attr('href', oldHref.replace('/' + oldFid + '/', '/' + mediaFile.fid + '/'));
-            imagecropLink.show();
-            
-          }
-          
-        }, globalOptions);
-        return false;
-      });
-  
-      // When someone clicks the Remove button.
-      $('.remove', this).bind('click', function () {
-        // Set the value of the filefield fid (hidden).
-        fidField.val(0);
-        // Set the preview field HTML.
-        previewField.html('');
-        // Hide the Remove button.
-        removeButton.hide();
-        return false;
-      });
-  
-      $('.media-edit-link', this).bind('click', function () {
-        var fid = fidField.val();
-        if (fid) {
-          Drupal.media.popups.mediaFieldEditor(fid, function (r) { alert(r); });
-        }
-        return false;
-      });
-  
-    });
-    
+  	$('.media-widget', context).find('.fid').bind('change', Drupal.Imagecrop.mediaChangeListener);
   }
   
 }
 
-Drupal.Imagecrop = {}
+/**
+ * Listener on a media field.
+ */	
+Drupal.Imagecrop.mediaChangeListener = function(e) {
+	
+	var $this = $(this);
+	var $mediaWrapper = $this.parent();
+	var $imagecropButton = $('.imagecrop-button', $mediaWrapper);
+	
+	if ($this.val() != 0) {
+	  
+	  var $imagecropUrl = $('.imagecrop-url', $mediaWrapper);
+		$imagecropButton.css({display : 'inline-block'});
+
+		// Set the new url
+    $imagecropButton.attr('href', $imagecropUrl.val().replace('/fid/', '/' + $this.val() + '/'));
+    
+	}
+	else {
+		$imagecropButton.hide();
+	}
+		
+}
 
 /**
  * Open the imagecrop popup to the given link
