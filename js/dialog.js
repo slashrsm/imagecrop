d8496c69b462e6e5dde6967b0dc63eb436f9e49b
zuuperman <nils.destoop@gmail.com>
2012-10-29 09:42:47 +0100

    Issue #1728694 by jcfiala: Fixed Multiple Dialogs opening when Crop Image is clicked.

--- js/dialog.js
+++ js/dialog.js
@@ -9,7 +9,7 @@
   Drupal.behaviors.imagecropDialog = {
     attach: function(context) {
       // Add dialog listener on imagecrop links.
-      $(context).find('a.imagecrop-button').bind('click', Drupal.Imagecrop.openDialog);
+      $(context).find('a.imagecrop-button').once('imagecrop').bind('click', Drupal.Imagecrop.openDialog);
     }
   }
   
@@ -21,15 +21,22 @@
     e.preventDefault();
     var page = $(this).attr("href");
     var pagetitle = $(this).attr("title");
-    Drupal.Imagecrop.dialogContainer = $('<div id="imagecrop-modal-container"></div>')
+    if (Drupal.Imagecrop.dialogContainer == null) {
+      Drupal.Imagecrop.dialogContainer = $('<div id="imagecrop-modal-container"></div>');      
+    }
+    
+    var popup_height = $.isArray(Drupal.settings.imagecrop.popupHeight) ? Drupal.settings.imagecrop.popupHeight[0] : Drupal.settings.imagecrop.popupHeight;
+    var popup_width = $.isArray(Drupal.settings.imagecrop.popupWidth) ? Drupal.settings.imagecrop.popupWidth[0] : Drupal.settings.imagecrop.popupWidth;
+    
+    Drupal.Imagecrop.dialogContainer
       .html('<iframe style="border: 0px; " src="' + page + '" width="100%" height="100%"></iframe>')
       .dialog({
         autoOpen: true,
         closeOnEscape : false,
         dialogClass : 'imagecrop-dialog',
         modal: true,
-        height: Drupal.settings.imagecrop.popupHeight,
-        width: Drupal.settings.imagecrop.popupWidth,
+        height: popup_height,
+        width: popup_width,
         title: pagetitle
       });
     
